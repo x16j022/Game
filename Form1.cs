aabde832d85ba121b23ca547bda2f8a855e899fb
WIN_NT\x16j022 <x16j022@chiba-fjb.ac.jp>
2017-06-02 11:56:57 +0900

    Game

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,46 @@ namespace WindowsFormsApp5
 {
     public partial class Form1 : Form
     {
+        int speed = 10;
+        int moveX;
+        int moveY;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Left += speed;
+            moveX += speed;
+            moveY = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= speed;
+            moveY -= speed;
+            moveX = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Top += speed;
+            moveY += speed;
+            moveX = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= speed;
+            moveX -= speed;
+            moveY = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += moveX;
+            pictureBox1.Top += moveY;
         }
     }
 }
